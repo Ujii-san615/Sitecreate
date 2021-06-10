956fb58cf6adc1f4e67322920803245b4069e181
Chiaki Ujiie <ujiie.c615@gmail.com>
2021-06-10 16:00:51 +0900

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,36 @@
-$(function(){
-    $("#bgndVideo").YTPlayer();
-    $(document).ready(function(){
-        $("#bgVideo").YTPlayer();
+$(function () {
+/* youtube player */
+
+    // $("#bgndVideo").YTPlayer();
+    // $(document).ready(function () {
+    //     $("#bgVideo").YTPlayer();
+
+/* header scroll hidden */
+    let start_position = 0,//初期位置０
+        window_position,
+        $window = $(window),
+        $header = $('header'),
+        $footer = $('footer');
+
+    // スクロールイベントを設定
+    $window.on('scroll', function () {
+        // スクロール量の取得
+        window_position = $(this).scrollTop();
+
+        // スクロール量が初期位置より小さければ，
+        // 上にスクロールしているのでヘッダーフッターを出現させる
+        if (window_position <= start_position) {
+            $header.css('top', '0');
+            $footer.css('bottom', '0');
+        } else {
+            $header.css('top', '-70px');
+            $footer.css('bottom', '-70px');
+        }
+        // 現在の位置を更新する
+        start_position = window_position;
     });
+    // 中途半端なところでロードされても良いようにスクロールイベントを発生させる
+    $window.trigger('scroll');
 });
+
 
